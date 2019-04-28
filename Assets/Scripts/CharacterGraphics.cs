f4e3229098826cbc0bb4d81da1641b0973ae6d25
AndreSchuch <andreschuchdev@gmail.com>
2019-04-28 09:51:53 -0300

    Implemented UI art partially

---
+++ Assets/Scripts/CharacterGraphics.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGraphics : MonoBehaviour
+{
+    public Renderer Renderer;
+
+    private Material material;
+
+    private float saturationAmount = 1;
+
+    private void Start()
+    {
+        material = Renderer.material;
+    }
+
+    private void Update()
+    {
+        var ambientColor = RenderSettings.ambientLight;
+        material.SetColor("_AmbientColor", ambientColor);
+        material.SetFloat("_SaturationAmount", saturationAmount);
+    }
+}
---
+++ Assets/Scripts/MainMenuButtom.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtom : MonoBehaviour
+{
+    public void OnResumeGameButton()
+    {
+        SceneManager.LoadScene("SCN_LobbyStage");
+    }
+
+    public void OnNewGameButton()
+    {
+        SceneManager.LoadScene("SCN_LobbyStage");
+    }
+
+    public void OnCreditsButton()
+    {
+
+    }
+
+    public void OnExitButton()
+    {
+        Application.Quit();
+    }
+}
